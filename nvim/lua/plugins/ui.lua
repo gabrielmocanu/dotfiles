20dd3c2437dabc79e5d8ab2fd300aa02a88e263e
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2025-05-23 11:48:59 +0300

    update neovim to use biome
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,5 @@
 return {
   { "rasulomaroff/reactive.nvim" },
   { "HiPhish/rainbow-delimiters.nvim" },
-  { "shortcuts/no-neck-pain.nvim", version = "*", opts = { width = 200, autocmds = { enableOnVimEnter = true } } },
+  { "shortcuts/no-neck-pain.nvim", version = "*", opts = { width = 300 } },
 }
