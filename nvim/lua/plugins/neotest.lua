4583d002a2f79e31876c8126bd39a974f10d6237
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2025-08-31 11:39:26 +0300

    chore: update
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -8,13 +8,22 @@ return {
       "antoinemadec/FixCursorHold.nvim",
       "nvim-neotest/neotest-python",
       "marilari88/neotest-vitest",
+      "jfpedroza/neotest-elixir",
     },
     config = function()
       require("neotest").setup({
         adapters = {
           require("neotest-python"),
           require("neotest-vitest")({
-            vitestCommand = "vitest",
+            vitestCommand = "pnpm --prefix assets run test:vitest",
+          }),
+          require("neotest-elixir")({
+            filter_dir = function(name, rel_path, root)
+              return rel_path == "test"
+                or rel_path == "lib"
+                or vim.startswith(rel_path, "test/")
+                or vim.startswith(rel_path, "lib/")
+            end,
           }),
         },
         icons = {
